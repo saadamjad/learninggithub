28582fb1d6f8d4fc71897baac4fa318ea2ef1a96
saadamjad <saad.amjad434@gmail.com>
2021-12-19 18:27:35 +0500

    2

--- App.js
+++ App.js
@@ -32,10 +32,10 @@ const App: () => Node = () => {
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <View style={styles.sectionContainer}>
-        <Text> develop branch 1</Text>
-        <Text> develop branch 2</Text>
-        <Text> develop branch 3</Text>
-        <Text> develop branch 4</Text>
+        <Text> develop branch 1,1</Text>
+        <Text> develop branch 2,2</Text>
+        <Text> develop branch 3,3</Text>
+        <Text> develop branch 4,4</Text>
       </View>
     </SafeAreaView>
   );
